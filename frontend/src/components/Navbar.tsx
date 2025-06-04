487145cb7903af1c8f7894e028384dbb4445b558
Aditya <pratapvalor.2023@gmail.com>
2025-06-04 14:32:03 +0530

    Responsive Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import{ NavLink } from 'react-router-dom'
+import { FaSearch,FaBars,FaTimes } from "react-icons/fa";
 
 function Navbar() {
+  const [searchText, setSearchText] = useState<string>('');
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
     <>
-     <nav className='bg-purple-300 border-2 border-gray-500 p-4'>
-        <ul className="flex gap-8 justify-center">
+     <nav className='bg-purple-300 border-2 border-gray-500 px-4 py-3 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+      <div className='flex items-center justify-between w-full md:w-auto'>
+        <div className='text-xl font-bold'>
+          LOGO
+        </div>
+          <button className='md:hidden text-2xl' onClick={()=> setIsMenuOpen(!isMenuOpen)}>
+            {isMenuOpen ? <FaTimes/> : <FaBars/>}
+          </button>
+      </div>
+      <div className='bg-white px-3 py-1 rounded-sm w-full flex gap-2 flex-row text-gray-500 md:w-1/3 items-center'>
+        <div><FaSearch />
+        </div>
+        <input 
+        type='text'
+        placeholder='Search for Products,Brands and More'
+        className='outline-none w-full text-black'
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+      </div>
+        <ul className={`flex flex-col md:flex-row gap-3 md:gap-8 text-center w-full md:w-auto px-20 ${isMenuOpen ? 'block' : 'hidden'} md:flex`}>
             <li><NavLink to="/" className={({ isActive }) => isActive ? 'text-purple-500': 'text-black'}>Home</NavLink></li>
             <li><NavLink to="/shop" className={({ isActive }) => isActive ? 'text-purple-500': 'text-black'}>Shop</NavLink></li>
             <li><NavLink to="/about" className={({ isActive }) => isActive ? 'text-purple-500': 'text-black'}>About</NavLink></li>
             <li><NavLink to="/contact" className={({ isActive }) => isActive ? 'text-purple-500': 'text-black'}>Contact</NavLink></li>    
-        </ul>    
+        </ul>
+
     </nav> 
     </>
   )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,10 +4,10 @@ import { CiShoppingCart } from "react-icons/ci";
 function Home() {
   return (
     <div className='border-3 border-purple-950 p-5 flex'>
-        <div className='w-1/2 flex flex-col gap-4 justify-center items-center text-center h-screen'>
+        <div className='w-1/2 flex flex-col gap-14 justify-center items-center text-center h-screen'>
             <p className='font-bold text-purple-400 font-serif text-8xl  '>Believes in Comfort!</p>
             <div className='bg-purple-600 rounded-full px-6 py-3 text-white flex items-center gap-2 hover:bg-purple-700 transition cursor-pointer'>
-                <CiShoppingCart />
+                <CiShoppingCart className='text-2xl'/>
                 <button >Shop Now</button>
             </div>
         </div>
